2a0bfffe79b1a90c5444cb071194661f3994b62c
Honza_Yang <yangzhaojun666@gmail.com>
2024-05-04 12:47:42 +0200

    some classes

---
+++ Hiderbot/Hiderbot/Class.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hiderbot
+{
+    internal class Class
+    {
+        public string ClassNumber { get; set; }
+        public List<Subjects> Subjects { get; set; }
+
+        public Class (string classNumber, List<Subjects> subjects)
+        {
+            ClassNumber = classNumber;
+            Subjects = subjects;
+        }
+
+
+    }
+}
---
+++ Hiderbot/Hiderbot/Teacher.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hiderbot
+{
+    internal class Teacher
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public List<Subjects> Subjects { get; set; }
+        public Teacher(string name, string surname, List<Subjects> subjects)
+        {
+            Name = name;
+            Surname = surname;
+            Subjects = subjects;
+        }
+    }
+}
