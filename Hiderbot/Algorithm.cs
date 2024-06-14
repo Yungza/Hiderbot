490d65bc71f0b0cb7435dc7bfbbbf3de2ff17e0c
cermafil <cermafil@gmail.com>
2024-06-14 15:32:18 +0200

    .

--- Hiderbot/Algorithm.cs
+++ Hiderbot/Algorithm.cs
@@ -47,7 +47,7 @@ namespace Hiderbot
             {
                 List<string> subjects = new List<string>();
 
-                // Loop through the Requirement dictionary of the class
+               
                 foreach (var subject in c.Requirement)
                 {
                     string subjectName = subject.Key;
@@ -65,25 +65,25 @@ namespace Hiderbot
                 subjectsAll.Add(subjects);
             }
 
-            var subjectTeacherMap = new Dictionary<string, List<Teacher>>();
+            var subjectTeacher = new Dictionary<string, List<Teacher>>();
 
-            // Prepare a map of subjects to teachers
+            
             foreach (var teacher in teachers)
             {
                 foreach (var subject in teacher.Subjects)
                 {
-                    if (!subjectTeacherMap.ContainsKey(subject))
+                    if (!subjectTeacher.ContainsKey(subject))
                     {
-                        subjectTeacherMap[subject] = new List<Teacher>();
+                        subjectTeacher[subject] = new List<Teacher>();
                     }
-                    subjectTeacherMap[subject].Add(teacher);
+                    subjectTeacher[subject].Add(teacher);
                 }
             }
 
             // Shuffle teachers for each subject group
-            foreach (var key in subjectTeacherMap.Keys.ToList())
+            foreach (var key in subjectTeacher.Keys.ToList())
             {
-                ListExtensions.Shuffle(subjectTeacherMap[key]);
+                ListExtensions.Shuffle(subjectTeacher[key]);
             }
 
             // Assign periods
@@ -93,9 +93,9 @@ namespace Hiderbot
 
                 foreach (var subject in subjectList)
                 {
-                    if (subjectTeacherMap.ContainsKey(subject))
+                    if (subjectTeacher.ContainsKey(subject))
                     {
-                        var availableTeachers = subjectTeacherMap[subject];
+                        var availableTeachers = subjectTeacher[subject];
                         foreach (var teacher in availableTeachers)
                         {
                             Period period = new Period(teacher, subject);
@@ -113,7 +113,7 @@ namespace Hiderbot
         public int CalculateFitness(List<List<Period>> scheduleAll)
         {
             int fitness = 100;
-            var teacherScheduleMap = new Dictionary<Teacher, HashSet<int>>();
+            var teacherSchedule = new Dictionary<Teacher, HashSet<int>>();
 
             for (int i = 0; i < scheduleAll.Count; i++)
             {
@@ -122,26 +122,26 @@ namespace Hiderbot
                     Period period = scheduleAll[i][j];
                     Teacher teacher = period.Teacher;
 
-                    if (!teacherScheduleMap.ContainsKey(teacher))
+                    if (!teacherSchedule.ContainsKey(teacher))
                     {
-                        teacherScheduleMap[teacher] = new HashSet<int>();
+                        teacherSchedule[teacher] = new HashSet<int>();
                     }
 
                     // If the teacher is already scheduled for this time slot, it's a conflict
-                    if (teacherScheduleMap[teacher].Contains(j))
+                    if (teacherSchedule[teacher].Contains(j))
                     {
                         fitness -= 10;
                     }
                     else
                     {
-                        teacherScheduleMap[teacher].Add(j);
+                        teacherSchedule[teacher].Add(j);
                     }
                 }
             }
 
             return fitness;
         }
-        public List<List<Period>> FindBestSchedule(List<Teacher> teachers, List<Class> classes, int iterations = 1)
+        public List<List<Period>> FindBestSchedule(List<Teacher> teachers, List<Class> classes, int iterations = 1000)
         {
             List<List<Period>> bestSchedule = null;
             int bestFitness = int.MinValue;
@@ -206,7 +206,7 @@ namespace Hiderbot
                 }
             }
 
-            // Distribute the extra items randomly
+            
             for (int i = 0; i < extraItems; i++)
             {
                 int randomIndex = random.Next(5);
